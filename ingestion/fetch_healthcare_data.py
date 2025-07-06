2f3569a694d20780e4dcd94d065ca63bdadd5cb9
PVSK <pvsk1303@gmail.com>
2025-07-06 16:21:00 -0400

    inital project setup

---
+++ ingestion/fetch_healthcare_data.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+URL = "https://data.cms.gov/provider-data/api/1/datastore/query/27ea-46a8/0"
+
+response = requests.get(URL)
+data = response.json()
+
+df = pd.json_normalize(data['results'])
+
+df.to_csv("../data/raw/hospital_data.csv", index=False)
+
+print("Raw healthcare data saved")
